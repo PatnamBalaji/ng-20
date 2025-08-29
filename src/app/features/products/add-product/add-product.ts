575d4439e82d1ddd4188953c8832fbfabc16aeb9
Balaji <balaji.patnam92@gmail.com>
2025-08-30 01:54:27 +0530

    init

--- src/app/features/products/add-product/add-product.ts
+++ src/app/features/products/add-product/add-product.ts
@@ -1,28 +1,28 @@
-import { Component } from "@angular/core";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { MatButtonModule } from "@angular/material/button";
+import { Component } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import {
   FormBuilder,
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
   Validators,
-} from "@angular/forms";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { CommonModule } from "@angular/common";
-import { MatCardModule } from "@angular/material/card";
-import { MatSelectModule } from "@angular/material/select";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { ActivatedRoute, RouterModule } from "@angular/router";
-import { products } from "../products.constants";
+} from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { products } from '../products.constants';
 
 @Component({
-  selector: "app-add-product",
-  templateUrl: "./add-product.html",
-  styleUrl: "./add-product.scss",
+  selector: 'app-add-product',
+  templateUrl: './add-product.html',
+  styleUrl: './add-product.scss',
   imports: [
     MatFormFieldModule,
     MatInputModule,
@@ -43,22 +43,22 @@ import { products } from "../products.constants";
 export class AddProduct {
   productForm: FormGroup;
   isLoading = false;
-  categories = ["Electronics", "Books", "Clothing", "Home Appliances"];
+  categories = ['Electronics', 'Books', 'Clothing', 'Home Appliances'];
   productId: string | null = null;
   constructor(private fb: FormBuilder, private route: ActivatedRoute) {
     this.productForm = this.fb.group({
-      name: ["", Validators.required],
-      description: ["", Validators.required],
+      name: ['', Validators.required],
+      description: ['', Validators.required],
       price: [null, [Validators.required, Validators.min(0)]],
       stock: [null, [Validators.required, Validators.min(0)]],
-      category: ["", Validators.required],
-      availableSince: [""],
+      category: ['', Validators.required],
+      availableSince: [''],
       active: [true],
     });
   }
 
   ngOnInit() {
-    const productId = this.route.snapshot.paramMap.get("id");
+    const productId = this.route.snapshot.paramMap.get('id');
 
     if (productId) {
       this.productId = productId;
@@ -72,7 +72,6 @@ export class AddProduct {
   onSubmit() {
     if (this.productForm.valid) {
       this.isLoading = true;
-
       // Simulate API call
       setTimeout(() => {
         console.log(this.productForm.value);
